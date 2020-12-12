f2c56d43476a2c95a198603513f64197fd7cb850
kinjo_s <hahaha.kinjo0330@gmail.com>
2020-12-12 12:03:40 +0900

    削除機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
   resources :posts
 
   # 以下は後々必要かも？
-  # get 'posts/index'
-  # get 'posts/show'
-  # get 'posts/new'
-  # get 'posts/create'
-  # get 'posts/edit'
-  # get 'posts/update'
-  # get 'posts/destroy'
+  get "posts/index"
+  get "posts/show"
+  get "posts/new"
+  get "posts/create"
+  get "posts/edit"
+  get "posts/update"
+  get "posts/destroy"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
